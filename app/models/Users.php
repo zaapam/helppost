063ceae5055518d9c440d1a6051e20df17965239
SettapakY <settapak@sanookonline.co.th>
2016-02-12 10:04:23 +0700

    Initialize project
    - Initialize project

---
+++ app/models/Users.php
@@ -0,0 +1,11 @@
+<?php
+
+use Phalcon\Mvc\Model;
+use Phalcon\Mvc\Model\Message;
+use Phalcon\Mvc\Model\Validator\Uniqueness;
+use Phalcon\Mvc\Model\Validator\InclusionIn;
+
+class Robots extends Model
+{
+
+}
